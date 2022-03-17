f476af859d7c87b3a48f7cdb7b82051b90d9310a
chellebernardo <michellebernardo13@hotmail.com>
2022-03-17 13:04:06 -0300

    Finalização semana 7, 8 e inicio da 9

---
+++ Alura/7 - Semana 007 e 008 rs/Fundamentos JavaScript 1, 2, 3/2/teste.js
@@ -0,0 +1,10 @@
+jantarDeHoje = ['lanche', 'hot dog', 'pizza']
+
+jantarDeHoje.pop()
+jantarDeHoje.pop()
+
+jantarDeHoje.push('ovo')
+jantarDeHoje.push('salada')
+jantarDeHoje.push('fruta')
+
+console.log(jantarDeHoje)
